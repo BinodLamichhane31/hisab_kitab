bec8132f89fd2308d5f46e2bd72edb15f1ac5779
binodlamiochhane31 <lamibinod31@gmail.com>
2025-07-09 20:40:14 +0545

    Added shops API endpoints and frontend services

---
+++ src/api/shopApi.js
@@ -0,0 +1,8 @@
+import axios from './api'
+
+export const createShopApi = async (data) => axios.post("/shops",data)
+export const getShopsApi = async () => axios.get('/shops')
+export const getShopByIdApi = async (id) => axios.get(`/shops/${id}`)
+export const updateShopApi = async (id, data) => axios.put(`/shops/${id}`,data)
+export const deleteShopApi = async (id) => axios.delete(`/shops/${id}`)
+export const selectActiveShopApi = async (shopID) => axios.post('/shops/select-shop',{shopID})
---
+++ src/services/shopService.js
@@ -0,0 +1,56 @@
+import { createShopApi, deleteShopApi, getShopByIdApi, getShopsApi, selectActiveShopApi, updateShopApi } from "../api/shopApi"
+
+export const createShopService = async(formData) =>{
+    try {
+        const response = await createShopApi(formData)
+        return response
+    } catch (error) {
+        throw error.response?.data || {message: "Failed to add shop"}
+    }
+}
+
+export const  getShopService = async () =>{
+    try {
+        const response = await getShopsApi()
+        return response
+    } catch (error) {
+        throw error.response?.data || {message: "Failed to fetch shops"}
+    }
+}
+
+export const getShopByIdService = async (id) =>{
+    try{
+        const response = await getShopByIdApi(id)
+        return response
+    } catch(error){
+        throw error.response?.data || {message: "Failed to fetch shop"}
+    }
+}
+
+export const updateShopService = async (id, data) =>{
+    try {
+        const response = await updateShopApi(id,data)
+        return response
+    } catch (error) {
+        throw error.response?.data || {message: "Failed to update shop"}
+    }
+}
+
+export const deleteShopService = async (id) =>{
+    try {
+        const response = await deleteShopApi(id)
+        return response
+    } catch (error) {
+        throw error.response?.data || {message: "Failed to delete shop"}
+    }
+}
+
+export const selectActiveShopService = async (shopId) =>{
+    try {
+        const response = await selectActiveShopApi(shopId)
+        return response
+    } catch (error) {
+        throw error.response?.data || {message: "Failed to switch shop"}
+    }
+}
+
