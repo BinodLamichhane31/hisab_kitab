3e24d0ce6f07be431926be9eccd88b1172d04e00
BinodLamichhane31 <lamibinod31@gmail.com>
2025-07-31 22:50:11 +0545

    minor changes

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,3 +3,13 @@ export const registerUserApi = (data) => axios.post("/auth/register",data)
 export const loginUserApi = (data) => axios.post("/auth/login",data,{withCredentials:true})
 export const logoutUserApi = () => axios.post("/auth/logout", {}, { withCredentials: true });
 export const getProfileApi = () => axios.get("/auth/profile", { withCredentials: true });
+export const updateProfileApi = (data) => axios.put("/auth/profile", data, { withCredentials: true });
+
+export const uploadProfileImageApi = (formData) => {
+  return axios.put("/auth/upload-profile-image", formData, {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
